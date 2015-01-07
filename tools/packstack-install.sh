4c8c9c6892dbe0b699f4172f8a9b5550d35e87eb
galthaus <galthaus@austin.rr.com>
2015-01-07 09:10:28 -0600

    Make this understand develop/master

--- tools/packstack-install.sh
+++ tools/packstack-install.sh
@@ -3,12 +3,19 @@
 # Copyright 2014, Greg Althaus
 #
 
+TREE="master"
+REPO="el6"
+if [ "$1" == "--develop"]; then
+  TREE="develop"
+  REPO="el6" # One day support develop
+fi
+
 # Setup RackN public repo
 cd /etc/yum.repos.d
 cat > rackn_public.repo <<EOF
 [racknpublic]
 name=repo for rackn public rpms
-baseurl=http://rackn.s3-website-us-east-1.amazonaws.com/public/el6
+baseurl=http://rackn.s3-website-us-east-1.amazonaws.com/public/$REPO
 enabled=1
 gpgcheck=0
 type=none
@@ -20,7 +27,7 @@ cd -
 #
 # Make sure the opencrowbar is installed
 #
-wget --no-check-certificate -O - https://raw.githubusercontent.com/opencrowbar/core/master/tools/crowbar-install.sh | source /dev/stdin $@
+wget --no-check-certificate -O - https://raw.githubusercontent.com/opencrowbar/core/$TREE/tools/crowbar-install.sh | source /dev/stdin $@
 
 # Install code
 yum install -y rackn-packstack
